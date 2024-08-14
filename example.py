b5f95d0fd6274b8cc0e52d9bd6be99f3700f27a5
Adam <thomasadam83@hotmail.com>
2024-08-14 14:21:23 -0400

    Added sample

---
+++ example.py
@@ -0,0 +1,17 @@
+from pybricks.tools import wait
+
+import bluetooth
+
+print("Hello World")
+
+wait(2000)
+
+print("Hello Adain")
+
+wait(2000)
+
+
+
+# https://github.com/pybluez/pybluez
+# https://pybricks.com/project/pybricks-other-editors/
+# https://stackoverflow.com/questions/21000680/how-to-find-visible-bluetooth-devices-in-python
